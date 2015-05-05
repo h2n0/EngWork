7466c90a39292174b5b0d007c6bba5acf9df64eb
Elliot Lee-Cerrino <elc954@hotmail.co.uk>
2015-05-05 19:48:34 +0000

    Final cam.py update

--- cam.py
+++ cam.py
@@ -1,13 +1,13 @@
 from LCD import *
 import RPi.GPIO as GPIO
 import time as t
-import subprocess as sp
+import subprocess as sub
 
-version = "0.2"
+version = "0.3A"
 lcd = LCD()
 h = GPIO.HIGH
 l = GPIO.LOW
-
+delay = 0.4
 encoderA = 16
 encoderB = 21
 encoderButton = 20
@@ -44,24 +44,30 @@ def getCurrentTime():
 	return t.time()
 	
 def init():
-	lcd.message("Timelaspe\nCommand v{}".format(version))
+	lcd.message("Timelaspe\nCommand V{}".format(version))
 	t.sleep(3)
 	wipe()
 	draw()
 
 def moveLeft(d):
 	global motorL
-	print("Motor Left")
 	GPIO.output(motorL,GPIO.HIGH)
 	t.sleep(d)
 	GPIO.output(motorL,GPIO.LOW)
 
+def revolveMotorLeft(r):
+	delta = r * 30
+	moveLeft(delta)
+
 def moveRight(d):
 	global morotR
-	print("Motor Right")
 	GPIO.output(motorR,GPIO.HIGH)
 	t.sleep(d)
 	GPIO.output(motorR,GPIO.LOW)
+
+def revolveMotorRight(r):
+	delta = r * 30
+	moveRight(delta)
 
 def updateEncoder(c):
 	global encoderA, encoderB, lastencoded, encoderVal
@@ -72,14 +78,15 @@ def updateEncoder(c):
 
 	if(GPIO.input(encoderB)):
 		rotb = 1
-	
+
+	dec = 2	
 	rotc = rota ^ rotb
 	encoded = rota * 4 + rotb * 2 + rotc * 1
 	delta = (encoded - lastencoded)
 	if(delta == 1):
-		encoderVal = encoderVal + 1
+		encoderVal = encoderVal + dec
 	elif(delta == 3):
-		encoderVal = encoderVal - 1
+		encoderVal = encoderVal - dec
 
 	lastencoded = encoded
 
@@ -96,24 +103,35 @@ def button(c):
 	elif(stage == 3):
 		op = encoderVal % 2
 		if(op == 0):
+			totalFrames = FPH * H
+			print("FPH: {}, H: {}, TF: {}".format(FPH,H,totalFrames))
 			setup = False
 			stage = 0
 			encoderVal = 0
-			totalFrame = FPH * H
 		else:
 			stage = 1
 			encoderVal = 0
 
+def getStringPercent(f,t):
+	res = " [            ] "
+	p = ((f/t) * 12)
+	while p > 0:
+		res[1+p] = "="
+	return res
+
+def compile():
+	sub.call("ls *.png > stills.txt")
+	sub.call("mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=png:fps=24 mf://@stills.txt")
+
 def draw():
-	global encoderVal,encoderA,encoderB, stage, FPH, H, setup
-	sp.call("rm pics/*.png")
+	global encoderVal,encoderA,encoderB, stage, FPH, H, setup,delay,currentFrame,totalFrames
+	sub.call("rm Pic*.png",shell=True)
 	GPIO.add_event_detect(encoderA,GPIO.BOTH,callback=updateEncoder)
 	GPIO.add_event_detect(encoderB,GPIO.BOTH,callback=updateEncoder)
 	GPIO.add_event_detect(encoderButton,GPIO.FALLING,callback=button,bouncetime=300)
 	try:
 		while True:
 			wipe()
-			#button = GPIO.input(encoderButton)
 			if setup:
 				if stage == 1:#FPH setup
 					FPH = encoderVal
@@ -134,16 +152,24 @@ def draw():
 				## 7.5cm = motorRight(60)
 				## 1cm = motorRight(60/7.5)
 				## (60/7.5)/ totalFrames 
-				start = getCurrentTime()
-				cf = "-o pics/picture{}.png".format(currentFrame)
-				cmd = sp.call(["raspistill","-vf","-hf",cf], shell=True)
-				
-				t.sleep((getCurrentTime() - start) * (H/FPH))
-
-			t.sleep(0.3)
+				## Time delay =  60/FPH
+				framesLeft = "Pics left: {}".format(totalFrames - currentFrame)
+				lcd.message(framesLeft)#+ "\n"+getStringPercent(currentFrame,totalFrames))
+				start = t.time()
+				cf = "-o Pic{}.png".format(currentFrame)
+				sub.call("raspistill -hf " + cf,shell=True)
+				d = (75/2) / totalFrames
+				revolveMotorLeft(d)
+				t.sleep((60 * 60)/FPH - ((t.time() - start)))
+				currentFrame = currentFrame + 1
+				if currentFrame > totalFrames:
+					break
+	
+			t.sleep(delay)
+		compile()
 	finally:
 		GPIO.cleanup()
-			
-	
+
+
+revolveMotorLeft(0.5)
 init()
-print("Test")
